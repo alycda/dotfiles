1d79ba1fba4451f1440906ed7a4c84c12dfcee8e
Alyssa <alyssa@ditto.com>
2026-01-01 22:51:13 -0800

    Improve VSCode configuration settings

--- home-manager/modules/ide/vscode-profiles/base.nix
+++ home-manager/modules/ide/vscode-profiles/base.nix
@@ -2,14 +2,22 @@
 {
   userSettings = {
     chat.agent.enabled = false;
+    # claudeCode = {
+    #   preferredLocation = "panel";
+    # };
     claudeCode.preferredLocation = "panel";
+    diffEditor.ignoreTrimWhitespace = false;
     editor.wordWrap = "on";
     files.exclude = {
       "**/.jj" = true;
+      "result" = true;
     };
     search.exclude = {
       "**/.jj" = true;
+      "result" = true;
     };
+    # Use system zsh instead of nix-managed path that may not exist
+    terminal.integrated.defaultProfile.osx = "zsh";
     workbench.secondarySideBar.defaultVisibility = "hidden";
   };
 
