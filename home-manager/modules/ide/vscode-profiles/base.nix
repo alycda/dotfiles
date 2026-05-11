91e5ab90578bfc1348582e4447176a9f57fdf7b5
Alyssa Evans <alyda@me.com>
2026-05-11 08:50:18 -0700

    silence noisy suggestion by accepting it

--- home-manager/modules/ide/vscode-profiles/base.nix
+++ home-manager/modules/ide/vscode-profiles/base.nix
@@ -33,6 +33,7 @@
     vscode-marketplace.alefragnani.bookmarks
     vscode-marketplace.anthropic.claude-code # unfree
     vscode-marketplace.jnoortheen.nix-ide
+    vscode-marketplace.ms-azuretools.vscode-containers
     vscode-marketplace.ms-vscode-remote.remote-containers
     vscode-marketplace.ms-vscode-remote.remote-ssh
     vscode-marketplace.skellock.just
